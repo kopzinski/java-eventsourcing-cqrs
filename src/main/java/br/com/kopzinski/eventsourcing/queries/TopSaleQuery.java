b2fbae44f27e21395ddaa7ae972c230e5240ad4d
mellop <mellop@dbserver.com.br>
2020-12-15 17:35:27 -0300

    feat(OUT): Query objects

---
+++ src/main/java/br/com/kopzinski/eventsourcing/queries/TopSaleQuery.java
@@ -0,0 +1,12 @@
+package br.com.kopzinski.eventsourcing.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TopSaleQuery {
+
+    private String fileName;
+
+}
---
+++ src/main/java/br/com/kopzinski/eventsourcing/queries/WorseSellerQuery.java
@@ -0,0 +1,12 @@
+package br.com.kopzinski.eventsourcing.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WorseSellerQuery {
+
+    private String fileName;
+
+}
